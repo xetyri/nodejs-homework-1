2b78272d883dbf35a0d7000fd84cc881c6fae62b
Roman Leshchenko <xetyri2014@gmail.com>
2022-01-04 14:04:51 +0200

    Add code

---
+++ index.js
@@ -0,0 +1,55 @@
+const contactsOperations = require("./db/contact");
+const { program } = require("commander");
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await contactsOperations.listContacts();
+      console.table(contacts);
+      break;
+
+    case "get":
+      const contact = await contactsOperations.getContactById(id);
+      console.table(contact);
+      break;
+
+    case "add":
+      const newContact = await contactsOperations.addContact({
+        name,
+        email,
+        phone,
+      });
+      console.table(newContact);
+      break;
+
+    case "update":
+      const updContact = await contactsOperations.updateContact({
+        id,
+        name,
+        email,
+        phone,
+      });
+      console.table(updContact);
+      break;
+
+    case "remove":
+      const removedContact = await contactsOperations.removeContact(id);
+      console.table(removedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+program.parse(process.argv);
+const argv = program.opts();
+
+invokeAction(argv);
